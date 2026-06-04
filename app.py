8b9a75149e598afee6d075ffe7eb2db8332757e5
jash8866 <jash.ramani8866@gmail.com>
2026-06-04 15:26:14 +0530

    conn_obj

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 from ai import AIServiceError, talk
+from dbcon import create_connection
 
 app = FastAPI()
-
+db_connection = create_connection(host="localhost", username="root", password="", db_name="royalpms/royalpms_cryst8000")
 @app.get("/")
 def read_root():
     return {"message": "Hello world"}
