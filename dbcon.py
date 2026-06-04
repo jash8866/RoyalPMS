3f9b4721f18a968c84f21940583e2909a9a8c3d1
jash8866 <jash.ramani8866@gmail.com>
2026-06-04 15:21:08 +0530

    dbconn-1

--- dbcon.py
+++ dbcon.py
@@ -1,2 +1,16 @@
 import mysql.connector
 from mysql.connector import Error
+
+def create_connection(host, username, password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            user=username,
+            passwd=password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
